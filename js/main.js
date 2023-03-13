f7d09a4a310ca2b3078b55848bc79ea7d4cd0cce
LeoSallu <L.sallustio99@gmail.com>
2023-03-13 16:10:53 +0100

    Bonus es completato

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      img:'https://www.thesun.co.uk/wp-content/uploads/2022/08/553fc8c0-4e02-4a96-800b-a2453827fe2b.jpg'
     }
   },
   methods:{
